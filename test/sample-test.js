8c0be6d5b1a128191c39e45ef8ae6f88a715465c
lebonYG <lebonganza@outlook.com>
2021-11-26 00:26:36 -0500

    Added tests for contracts

---
+++ test/sample-test.js
@@ -0,0 +1,50 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("NFTMarket", function () {
+  it("Should create and execute market sales", async function () {
+    const Market = await ethers.getContractFactory("NFTMarket")
+    const market = await Market.deploy()
+    await market.deployed()
+    const marketAddress = market.address
+
+    const NFT = await ethers.getContractFactory("NFT")
+    const nft = await NFT.deploy(marketAddress)
+    await nft.deployed()
+    const nftContractAddress = nft.address
+
+    let listingPrice = await market.getListingPrice()
+    listingPrice = listingPrice.toString()
+
+    const auctionPrice = ethers.utils.parseUnits('100', 'ether')
+
+    await nft.createToken('https://www.mytokenlocation.com')
+    await nft.createToken('https://mytokenlocation2.com')
+
+
+    await market.createMarketItem(nftContractAddress, 1, auctionPrice, {value: listingPrice})
+    await market.createMarketItem(nftContractAddress, 2, auctionPrice, {value: listingPrice})
+
+
+ 
+    const [_, buyerAddress] = await ethers.getSigners()
+
+    await market.connect(buyerAddress).createMarketSell(nftContractAddress, 1, {value: auctionPrice})
+
+    let items = await market.fetchMarketItems()
+
+    items = await Promise.all(items.map(async i => {
+      console.log(i.tokenId.toString())
+      const tokenURI = await nft.tokenURI(i.tokenId)
+
+      return {
+        price: i.price.toString(),
+        tokenId: i.tokenId.toString(),
+        seller: i.seller,
+        owner: i.owner,
+        tokenURI
+      }
+    }))
+    console.log(items)
+  });
+});
